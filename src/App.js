6187d0dc595db9f5d8d0126930acb73cd289c1e9
Ben Shu <ben.shu@payscale.com>
2020-10-28 17:07:33 -0700

    init and gitignore

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <a href='http://localhost:8888'> Login to Spotify </a>
+    </div>
+  );
+}
+
+export default App;
